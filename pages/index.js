d0b085c48dd0f50ac86b102bbfccefd17fad125e
josezanle <jose.rios.lm@gmail.com>
2021-05-27 13:13:26 -0300

    testing seo

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import Banner from "../components/1-banner";
 
 import Loadable from "react-loadable";
 
-// import About from "../components/2-about";
-// import Slider from "../components/3-slider";
-// import Projects from "../components/4-projects";
-// import Faqs from "../components/5-faqs";
-// import Footer from "../components/footer";
+import About from "../components/2-about";
+import Slider from "../components/3-slider";
+import Projects from "../components/4-projects";
+import Faqs from "../components/5-faqs";
+import Footer from "../components/footer";
 import Loading from "../components/loading";
 
 const LoadableComponent = Loadable({
@@ -14,42 +14,42 @@ const LoadableComponent = Loadable({
   loading: Loading,
 });
 
-const LoadableComponent2 = Loadable({
-  loader: () => import("../components/2-about"),
-  loading: Loading,
-});
-const LoadableComponent3 = Loadable({
-  loader: () => import("../components/3-slider"),
-  loading: Loading,
-});
-const LoadableComponent4 = Loadable({
-  loader: () => import("../components/4-projects"),
-  loading: Loading,
-});
-const LoadableComponent5 = Loadable({
-  loader: () => import("../components/5-faqs"),
-  loading: Loading,
-});
-const LoadableComponent6 = Loadable({
-  loader: () => import("../components/footer"),
-  loading: Loading,
-});
+// const LoadableComponent2 = Loadable({
+//   loader: () => import("../components/2-about"),
+//   loading: Loading,
+// });
+// const LoadableComponent3 = Loadable({
+//   loader: () => import("../components/3-slider"),
+//   loading: Loading,
+// });
+// const LoadableComponent4 = Loadable({
+//   loader: () => import("../components/4-projects"),
+//   loading: Loading,
+// });
+// const LoadableComponent5 = Loadable({
+//   loader: () => import("../components/5-faqs"),
+//   loading: Loading,
+// });
+// const LoadableComponent6 = Loadable({
+//   loader: () => import("../components/footer"),
+//   loading: Loading,
+// });
 
 const index = () => {
   return (
     <>
       <LoadableComponent />
-      <LoadableComponent2 />
+      {/* <LoadableComponent2 />
       <LoadableComponent3 />
       <LoadableComponent4 />
       <LoadableComponent5 />
-      <LoadableComponent6 />
+      <LoadableComponent6 /> */}
 
-      {/* <About />
+      <About />
       <Slider />
       <Projects />
       <Faqs />
-      <Footer /> */}
+      <Footer />
     </>
   );
 };
