173349ce26c70a9534115279d4db5f0fe3aac84d
josezanle <jose.rios.lm@gmail.com>
2024-03-10 22:29:07 -0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,11 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta property="og:title" content="Zanle Studio" />
-      <meta property="og:type" content="blog" />
-      <meta property="og:image" content="https://res.cloudinary.com/dubv6xkxf/image/upload/v1710119832/l1rf6z3z5zytvhdqnuen.png" />
-      <meta property="og:url" content="https://zanle-studio.vercel.app/" />
-      <meta property="og:site_name" content="Zanle-Studio" />
+      <head>
+        <meta property="og:title" content="Zanle Studio" />
+        <meta property="og:type" content="blog" />
+        <meta property="og:image" content="https://res.cloudinary.com/dubv6xkxf/image/upload/v1710119832/l1rf6z3z5zytvhdqnuen.png" />
+        <meta property="og:url" content="https://zanle-studio.vercel.app/" />
+        <meta property="og:site_name" content="Zanle-Studio" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
