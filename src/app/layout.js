4d8b77387e3258208b3b04c8782b6c0531024260
josezanle <jose.rios.lm@gmail.com>
2024-03-10 22:40:41 -0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,11 @@ export default function RootLayout({ children }) {
         <meta property="og:image" content="https://res.cloudinary.com/dubv6xkxf/image/upload/v1710119832/l1rf6z3z5zytvhdqnuen.png" />
         <meta property="og:url" content="https://zanle-studio.vercel.app/" />
         <meta property="og:site_name" content="Zanle-Studio" />
+
+        {/* Metaetiquetas específicas para WhatsApp */}
+        <meta property="og:description" content="Somos una agencia de desarrollo de software, paginas web y aplicaciones android" />
+        <meta property="og:see_also" content="https://wa.me/+51925822952" />
+        <meta property="og:phone_number" content="+51925822952" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
