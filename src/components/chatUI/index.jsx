eb4899ba82d17fefa6429f836e4e68a81776b13b
josezanle <jose.rios.lm@gmail.com>
2026-02-28 13:00:37 -0300

    changin localhost to prodction url

--- src/components/chatUI/index.jsx
+++ src/components/chatUI/index.jsx
@@ -37,7 +37,7 @@ export default function ChatUi() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:8000/api/chat/", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/chat/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ session_key: sessionKey.current, message: text }),
